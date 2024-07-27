dd17d0658948997133b65a9d102571f10e9441cd
Farrel Dinarta <farrel.dinarta@ruparupa.com>
2024-07-27 23:45:56 +0700

    .

--- src/main/java/com/chesshouzs/server/repository/GameActiveRepository.java
+++ src/main/java/com/chesshouzs/server/repository/GameActiveRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.chesshouzs.server.model.GameActive;
 
 public interface GameActiveRepository extends JpaRepository<GameActive, UUID>, GameActiveRepositoryImpl {
-    @Query(nativeQuery = true, value = "SELECT * FROM game_active WHERE white_player_id = ?1 OR black_player_id = ?1 AND is_done = false LIMIT 1")
+    @Query(nativeQuery = true, value = "SELECT * FROM game_active WHERE (white_player_id = ?1 OR black_player_id = ?1) AND is_done = false LIMIT 1")
     GameActive findPlayerActiveMatch(UUID userId);
 }
 
