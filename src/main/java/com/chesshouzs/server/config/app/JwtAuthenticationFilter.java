0dc7b68c91d6cd14b28a6912f3fc9bf1aa963a97
Farrel Dinarta <farrel.dinarta@ruparupa.com>
2024-07-27 23:42:58 +0700

    chore : edit auth handling mechanism

--- src/main/java/com/chesshouzs/server/config/app/JwtAuthenticationFilter.java
+++ src/main/java/com/chesshouzs/server/config/app/JwtAuthenticationFilter.java
@@ -44,14 +44,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         @NonNull HttpServletResponse response,
         @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
-
-        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         try {
+            final String authHeader = request.getHeader("Authorization");
+
+            if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+                throw new Exception();
+            }
+
             final String jwt = authHeader.substring(7);
             final String userId = jwtService.extractId(jwt);
 
